e06e19d732a1ec5e36fa1b6e5fb8de3d6ffc36e3
Benjamin Haag <Benjamin.Haag@usda.gov>
2024-01-09 18:12:05 -0500

    Version 1.0.9 Beta: Taxon Editor, Note lookup fix.

---
+++ USDA.ARS.GRIN.GGTools.WebUI/Controllers/ImportController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using USDA.ARS.GRIN.GGTools.WebUI;
+using USDA.ARS.GRIN.GGTools.ViewModelLayer;
+using USDA.ARS.GRIN.GGTools.Taxonomy.ViewModelLayer;
+using USDA.ARS.GRIN.GGTools.Taxonomy.DataLayer;
+using NLog;
+
+namespace USDA.ARS.GRIN.GGTools.Taxonomy.WebUI.Controllers
+{
+    [GrinGlobalAuthentication]
+    public class ImportController : BaseController
+    {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        
+        public ActionResult Index(string eventAction = "", int folderId = 0)
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return RedirectToAction("InternalServerError", "Error");
+            }
+        }
+       
+    }
+}
