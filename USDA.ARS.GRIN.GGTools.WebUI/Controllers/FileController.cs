64ba6c8c07d5cf23ab49d9210ea0daacc8608059
Charles Benjamin Haag <benjamin.haag@usda.gov>
2026-03-11 18:55:10 -0400

    https://npgsweb.ars-grin.gov/trac/GG/ticket/3147

--- USDA.ARS.GRIN.GGTools.WebUI/Controllers/FileController.cs
+++ USDA.ARS.GRIN.GGTools.WebUI/Controllers/FileController.cs
@@ -20,46 +20,70 @@ namespace USDA.ARS.GRIN.GGTools.WebUI.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Upload(HttpPostedFileBase file)
         {
-            if (file == null || file.ContentLength == 0)
+            try
             {
-                ViewBag.Message = "No file provided.";
+                if (file == null || file.ContentLength == 0)
+                {
+                    ViewBag.Message = "No file provided.";
+                    return RedirectToAction("Index");
+                }
+
+                if (!Directory.Exists(_uploadPath))
+                    Directory.CreateDirectory(_uploadPath);
+
+                var safeName = Path.GetFileName(file.FileName);
+                var filePath = Path.Combine(_uploadPath, safeName);
+
+                file.SaveAs(filePath);
+
+                ViewBag.Message = safeName + " uploaded successfully.";
                 return RedirectToAction("Index");
             }
-
-            if (!Directory.Exists(_uploadPath))
-                Directory.CreateDirectory(_uploadPath);
-
-            var safeName = Path.GetFileName(file.FileName);
-            var filePath = Path.Combine(_uploadPath, safeName);
-
-            file.SaveAs(filePath);
-
-            ViewBag.Message = safeName + " uploaded successfully.";
-            return RedirectToAction("Index");
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return RedirectToAction("InternalServerError", "Error");
+            }
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(string fileName)
         {
-            var safeName = Path.GetFileName(fileName);
-            var filePath = Path.Combine(_uploadPath, safeName);
+            try
+            {
+                var safeName = Path.GetFileName(fileName);
+                var filePath = Path.Combine(_uploadPath, safeName);
 
-            if (System.IO.File.Exists(filePath))
-                System.IO.File.Delete(filePath);
+                if (System.IO.File.Exists(filePath))
+                    System.IO.File.Delete(filePath);
 
-            return RedirectToAction("Index");
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return RedirectToAction("InternalServerError", "Error");
+            }
         }
 
         public ActionResult Index()
         {
-            var files = Directory.Exists(_uploadPath)
-                ? Directory.GetFiles(_uploadPath)
-                           .Select(Path.GetFileName)
-                           .ToList()
-                : new List<string>();
+            try
+            {
+                var fileList = Directory.Exists(_uploadPath)
+                        ? Directory.GetFiles(_uploadPath)
+                       .Select(f => new FileInfo(f))
+                       .ToList()
+                        : new List<FileInfo>();
 
-            return View(files);
+                return View(fileList);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return RedirectToAction("InternalServerError", "Error");
+            }
         }
     }
 }
